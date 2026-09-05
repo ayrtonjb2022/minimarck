2a446c82d4fc2f27da4d902402d19fa8d03a75b8
Ayrton <benitezayrton.30@gmail.com>
2026-09-04 22:07:58 -0300

    cambio 0003

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
     dialect: "mysql",
     logging: process.env.NODE_ENV === "development" ? console.log : false,
     pool: {
-      max: 15,
+      max: 4,
       min: 0,
       acquire: 30000,
       idle: 10000,
